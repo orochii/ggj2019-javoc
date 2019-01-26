c2430197f292d893bdb70bc3d7a4ba968c80b66f
Orlando Marin Blanco <arcboundcrusher@gmail.com>
2019-01-26 07:15:57 -0600

    Unido cámara con personaje y algunas otras cosas.

---
+++ Javoc/Assets/scripts/MakeInvisible.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeInvisible : MonoBehaviour
+{
+    public class ObjetoEscondidoData {
+        public Transform obj;
+        public float ultimoCheck;
+        public Material mat;
+        public bool escondido;
+
+        public ObjetoEscondidoData(Transform _obj, float _check, Material _m) {
+            obj = _obj;
+            ultimoCheck = _check;
+            mat = _m;
+            escondido = true;
+        }
+    }
+
+    // Tag a buscar en objetos encontrados al hacer raycasting.
+    [SerializeField] string tagName = "Esconder";
+    [SerializeField] float checkTime = 0.1f;
+    private Transform _jugador;
+    List<ObjetoEscondidoData> objetosEscondidos;
+
+    void Start()
+    {
+        objetosEscondidos = new List<ObjetoEscondidoData>();
+        _jugador = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 direccion = _jugador.position - transform.position;
+        Ray ray = new Ray(transform.position, direccion);
+        RaycastHit[] hits = Physics.RaycastAll(ray, direccion.magnitude);
+        foreach(RaycastHit hit in hits) {
+            if (hit.transform.CompareTag(tagName)) {
+                // Esconde
+                EscondeObjeto(hit.transform);
+            }
+        }
+        //
+        foreach(ObjetoEscondidoData o in objetosEscondidos) {
+            if (o.escondido) {
+                float tiempoTranscurrido = Time.time - o.ultimoCheck;
+                if (tiempoTranscurrido > checkTime) AparecerObjeto(o);
+            }
+        }
+    }
+
+    void EscondeObjeto(Transform t) {
+        ObjetoEscondidoData objData = null;
+        foreach (ObjetoEscondidoData o in objetosEscondidos) {
+            if (o.obj == t) objData = o;
+            break;
+        }
+        if (objData == null) {
+            Material m = null;
+            MeshRenderer mr = t.GetComponent<MeshRenderer>();
+            if (mr != null) m = mr.material;
+            objData = new ObjetoEscondidoData(t, Time.time, m);
+            objetosEscondidos.Add(objData);
+        }
+        else {
+            objData.ultimoCheck = Time.time;
+            if (objData.escondido) return;
+            objData.escondido = true;
+        }
+        //
+        if (objData.mat != null) {
+            Debug.Log("Desaparece.");
+            StandardShaderUtils.ChangeRenderMode(objData.mat, StandardShaderUtils.BlendMode.Transparent);
+            Color c = objData.mat.color;
+            c.a = 0.5f;
+            objData.mat.color = c;
+        }
+    }
+
+    void AparecerObjeto(ObjetoEscondidoData objData) {
+        if (objData.mat != null) {
+            Debug.Log("Reaparece.");
+            StandardShaderUtils.ChangeRenderMode(objData.mat, StandardShaderUtils.BlendMode.Opaque);
+            Color c = objData.mat.color;
+            c.a = 1f;
+            objData.mat.color = c;
+        }
+        objData.escondido = false;
+    }
+}
--- Javoc/Assets/scripts/PlayerMovement.cs
+++ Javoc/Assets/scripts/PlayerMovement.cs
@@ -6,10 +6,18 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]CharacterController controladorPer;
     [SerializeField] float velocidadMov = 2;
+
+    // Esta variable es usada para tener una referencia al gráfico, para rotarlo de acuerdo a la dirección de movimiento.
+    [SerializeField] Transform grafico;
+    // Se guarda una referencia a la cámara.
+    private Camera camaraPrincipal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Es buena práctica guardar la referencia en lugar de utilizar Camera.main cada frame, 
+        // digamos que no es muy eficiente para llamarlo dentro de un "update".
+        camaraPrincipal = Camera.main;
     }
 
     // Update is called once per frame
@@ -18,7 +26,21 @@ public class PlayerMovement : MonoBehaviour
         float aVertical = Input.GetAxis("Vertical");
         float aHorizontal = Input.GetAxis("Horizontal");
         Vector3 movimiento = new Vector3(aHorizontal, 0, aVertical);
+        // Rotar movimiento de acuerdo a la cámara.
+        // Para hacer que el movimiento corresponda a la dirección de la cámara, vamos a ignorar todo excepto la rotación en el eje Y.
+        float rotacionYCam = camaraPrincipal.transform.rotation.eulerAngles.y;
+        // Se crea un cuaternión (clase que contiene una matriz de rotación) a partir de ángulos de rotación. Eje Y rota al personaje horizontalmente.
+        Quaternion rotacionCamara = Quaternion.Euler(0, rotacionYCam, 0);
+        movimiento = rotacionCamara * movimiento; // Para rotar un vector, multiplica el quaternion por el vector. It's magic.
+
         controladorPer.SimpleMove(movimiento*velocidadMov);
-        
+        // Rotar el personaje de acuerdo al movimiento.
+        // Si el vector es 0,0 tendrá magnitud 0, es mejor ignorarlo. sqrMagnitude es el cuadrado de la magnitud, lleva un cálculo menos -> es más liviano. :)
+        if (movimiento.sqrMagnitude > 0) {
+            // LookRotation básicamente crea una rotación que apunta hacia donde está mirando el vector que le pases.
+            Quaternion direccion = Quaternion.LookRotation(movimiento);
+            grafico.rotation = direccion; // Y aquí se asigna la dirección directamente al gráfico.
+        }
+        // POR HACER: Animar al personaje. Las animaciones de correr y de estar parado están hechas. :D
     }
 }
